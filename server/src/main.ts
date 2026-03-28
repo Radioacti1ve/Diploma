a68f149680b6245a8ecfdcdd5e7ce7b41c753664
Maxim Kornev <radioactiive@yandex.ru>
2026-03-28 23:16:52 +0300

    Fix

--- server/src/main.ts
+++ server/src/main.ts
@@ -29,6 +29,6 @@ async function bootstrap() {
 
 	app.disable('x-powered-by')
 
-	await app.listen(process.env.PORT || 3000, '0.0.0.0')
+	await app.listen(process.env.PORT || 4200, '0.0.0.0')
 }
 bootstrap()
