04e6346baad69201c182bfb25a1922150df2e2de
Maxim Kornev <radioactiive@yandex.ru>
2026-04-08 21:22:02 +0300

    Поправляет поиск

--- max-video-client/src/app/(public)/s/SearchPage.tsx
+++ max-video-client/src/app/(public)/s/SearchPage.tsx
@@ -32,8 +32,8 @@ export function SearchPage() {
 						count={6}
 						className='h-36 rounded-md'
 					/>
-				) : data?.videos.length ? (
-					data.videos.map(video => (
+				) : data?.data.videos.length ? (
+					data.data.videos.map(video => (
 						<VideoItem
 							key={video.id}
 							video={video}
